aa621cc84142e566fa3865602af3f737e1a2a584
Eitan Ziv <eitan@ziv.co>
2018-01-25 19:50:47 +0200

    moved main into ColorGenerator

--- homework4/ColorGenerator.java
+++ homework4/ColorGenerator.java
@@ -63,4 +63,18 @@ public class ColorGenerator extends Observable{
         if (colorGenerator == null) colorGenerator = new ColorGenerator();
         return colorGenerator;
     }
+    
+    public static void main(String[] args) {
+        ColorGenerator generator = ColorGenerator.getInstance();
+        Billboard rowsBillboard = new Billboard(new RowsStrategy());
+        Billboard columsBillboard = new Billboard(new ColumsStrategy());
+        Billboard twoRoundsBillboard = new Billboard(new TwoRoundsStrategy());
+        Billboard randomBillboard = new Billboard(new RandomStrategy());
+
+        generator.addObserver(rowsBillboard);
+        generator.addObserver(columsBillboard);
+        generator.addObserver(twoRoundsBillboard);
+        generator.addObserver(randomBillboard);
+
+    }
 }
